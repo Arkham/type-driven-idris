0a1fbdaf0ddc58472a28d7fa3af958543f752a20
Ju Liu <ju@noredink.com>
2021-08-20 01:08:08 +0100

    Wrap idris with rlwrap

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
-{ ... }:
 let
   sources = import ./nix/sources.nix { };
-  nixpkgs = import sources.nixpkgs { };
-in with nixpkgs; mkShell { buildInputs = [ idris ]; }
+  pkgs = import sources.nixpkgs { };
+  wrapped = pkgs.writeShellScriptBin "idris2" ''
+    exec ${pkgs.rlwrap}/bin/rlwrap ${pkgs.idris2}/bin/idris2 "$@"
+  '';
+  idris2 = pkgs.symlinkJoin {
+    name = "idris2";
+    paths = [ wrapped pkgs.idris2 ];
+  };
+in pkgs.mkShell { buildInputs = [ idris2 ]; }
